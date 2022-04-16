91c41ae8874b5370849282f64bc50c5604d6e9ef
KROKS13kir <arbitr13adv@gmail.com>
2022-04-16 20:17:06 +0300

    Демо-проект

--- app.py
+++ app.py
@@ -9,29 +9,35 @@ with open("candidates.json", "r", encoding="utf-8") as cand_file:
 
 @app.route("/")
 def all_cand():
+    result = '<pre>'
     for i in range(len(candidates_mas)):
-        print(f"<pre>"
-              f"Имя кандидата - {candidates_mas[i]['name']}\n"
-              f"Позиция кандидата - {candidates_mas[i]['position']}\n"
-              f"Навыки - {candidates_mas[i]['skills']}</pre>"
-              )
+        result += (f"<pre>"
+                   f"Имя кандидата - {candidates_mas[i]['name']}\n"
+                   f"Позиция кандидата - {candidates_mas[i]['position']}\n"
+                   f"Навыки - {candidates_mas[i]['skills']}\n\n"
+                   )
+    result += '<pre>'
+    return result
 
 
-@app.route("/candidates/<int:x>/")
+@app.route("/candidates/<int:x>")
 def candidate(x):
-    return f"<img src='{candidates_mas[{x}]['position']}'>\n<pre>Имя кандидата - {candidates_mas[{x}]['name']}\nПозиция кандидата - {candidates_mas[{x}]['position']}\nНавыки - {candidates_mas[{x}]['skills']}</pre>"
+    temp = candidates_mas[x]['picture'].replace('"', '')
+    return f"<img src= '{temp}'>\n<pre>Имя кандидата - {candidates_mas[x]['name']}\nПозиция кандидата - {candidates_mas[x]['position']}\nНавыки - {candidates_mas[x]['skills']}</pre>"
 
 
-#
-@app.route("/skills/<x>")
+@app.route("/skills/<x>/")
 def page_feed(x):
+    result = '<pre>'
     for i in range(len(candidates_mas)):
-        if x.lower() in candidates_mas[i]['skills'].lower():
-            print(f"<pre>"
-                  f"Имя кандидата - {candidates_mas[i]['name']}\n"
-                  f"Позиция кандидата - {candidates_mas[i]['position']}\n"
-                  f"Навыки - {candidates_mas[i]['skills']}</pre>"
-                  )
+        cand_skills = candidates_mas[i]['skills'].lower().split(', ')
+        if x in cand_skills:
+            result += (f"Имя кандидата - {candidates_mas[i]['name']}\n"
+                       f"Позиция кандидата - {candidates_mas[i]['position']}\n"
+                       f"Навыки - {candidates_mas[i]['skills']}\n\n"
+                       )
+    result += '<pre>'
+    return result
 
 
 app.run()
